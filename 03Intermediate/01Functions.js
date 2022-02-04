fa795c5c165e2c82ced496656686b41516cfcfed
ANJANI-MAADHURI <mchunduri.sap@gmail.com>
2022-02-04 07:18:02 +0530

    examples of basic functions

--- 03Intermediate/01Functions.js
+++ 03Intermediate/01Functions.js
@@ -1,13 +1,31 @@
 // basic function
-function sayHello() {
-    console.log("Hey there, Madhuri");
-}
+// function sayHello(name) {
+//     console.log("Hey there, Madhuri");
+//     // console.log("Hey there, " + name);
+//     console.log(`Howdy, ${name}.  Wo ist du?`);
+// }
+
+// // sayHello();
+// sayHello("AnjaniMadhuri");
+
+
 
 // Both the syntaxes are valid
 // Calling a function: we use () ex: sayHello()
 // Referenceing a function: we dont use (). Ex: sayHello
-sayHello();
-console.log("sayHello(): We are calling a function here");
-sayHello;
-console.log("sayHello: We are REFERNCING a function here");
+// sayHello();
+// console.log("sayHello(): We are calling a function here");
+// sayHello;
+// console.log("sayHello: We are REFERNCING a function here");
+
+
+// function with "reurn"
+function namstey(){
+    return "Namaskara from Bengaluru";
+}
+
+var greetings = namstey();
+
+console.log(greetings);
+console.log(namstey());
 
