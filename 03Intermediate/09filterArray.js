39d977c152214f921d4c7c6024619a3ce90d3135
ANJANI-MAADHURI <mchunduri.sap@gmail.com>
2022-02-05 23:15:38 +0530

    fill and filter in JS

---
+++ 03Intermediate/09filterArray.js
@@ -0,0 +1,10 @@
+var testArray = [ 2, 4, 6, 3, 5, 9, 8, 10, 11 ];
+// Fill: Value; start position, end position
+console.log(testArray.fill(0));
+console.log(testArray.fill("M", 3));
+console.log(testArray.fill("p" , 3, 6));
+
+const myNumber = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
+
+const result = myNumber.filter( (num) => num != 79 );
+console.log(result);
